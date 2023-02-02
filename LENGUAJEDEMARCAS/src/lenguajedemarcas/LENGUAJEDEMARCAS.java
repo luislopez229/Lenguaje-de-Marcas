5558b8cfca81e6e8bb2f9ec0fd7ca3540a413649
Luis López <luilopez229@gmail.com>
2023-02-02 19:36:26 +0100

    KMKMKMN

---
+++ LENGUAJEDEMARCAS/src/lenguajedemarcas/LENGUAJEDEMARCAS.java
@@ -0,0 +1,16 @@
+package lenguajedemarcas;
+
+/**
+ *
+ * @author LUIS
+ */
+public class LENGUAJEDEMARCAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
